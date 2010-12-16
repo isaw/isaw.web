2a12422fd4c38ce8ca8cc8ee203d1e4b941bbfc5
Christopher Warner <christopher.warner@nyu.edu>
2010-12-16 16:25:28 -0700

    Adding folderview interface

---
+++ src/isaw.events/isaw/events/interfaces/folderview.py
@@ -0,0 +1,4 @@
+from zope.interface import Interface                                                                                                               
+class IFolderView(Interface):
+    """Folder View.
+    """
