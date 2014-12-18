3ae50b577bcd76076d035217fa8c3c87b6fc0f5b
Alec Mitchell <alecpm@gmail.com>
2014-12-17 16:49:46 -0800

    Make it all unicode.  Fix logo.

--- src/isaw.policy/src/isaw/policy/metadata.py
+++ src/isaw.policy/src/isaw/policy/metadata.py
@@ -4,9 +4,9 @@ from plone.app.layout.viewlets import ViewletBase
 from collective.contentleadimage.config import IMAGE_FIELD_NAME
 
 
-TYPE_DEFAULT = 'article'
+TYPE_DEFAULT = u'article'
 TYPE_MAP = {
-    'Folder': '',
+    u'profile': u'profile'
 }
 
 
@@ -15,45 +15,64 @@ class OpenGraphTagViewlet(ViewletBase):
     """Viewlet which renders opengraph metadata for Facebook, etc."""
 
     def update(self):
-        portal_state = self.context.restrictedTraverse('@@plone_portal_state')
+        portal_state = self.context.restrictedTraverse(
+            '@@plone_portal_state')
+        context_state = self.context.restrictedTraverse(
+            '@@plone_context_state')
         self.portal = portal_state.portal()
 
         self.metatags = []
-        self.metatags.extend([('og:title', self.title),
-                              ('og:url', self.context.absolute_url()),
-                              ('og:image', self.image_url),
-                              ('og:site_name', self.sitename),
-                              ('og:description', self.description)])
+        self.metatags.extend([(u'og:title', self.title),
+                              (u'og:url', self.context.absolute_url()),
+                              (u'og:image', self.image_url),
+                              (u'og:site_name', self.sitename),
+                              (u'og:description', self.description)])
 
-        ctype = TYPE_MAP.get(self.context.portal_type, None)
-        ctype = TYPE_DEFAULT if ctype is None else ctype
-        if ctype:
-            self.metatags.append(('og:type', ctype))
+        if context_state.is_portal_root():
+            ctype = 'website'
+            if getattr(self.view, '__name__', None) == u'search':
+                self.metatags.append((u'og:type', u'article'))
+                self.metatags.append((u'article:section', u'search'))
+            else:
+                self.metatags.append((u'og:type', u'website'))
+        else:
+            ctype = TYPE_MAP.get(self.context.portal_type, None)
+            ctype = TYPE_DEFAULT if ctype is None else ctype
+            if ctype:
+                self.metatags.append((u'og:type', ctype))
 
-        if ctype == 'article':
+        modified_date = self.context.ModificationDate()
+        if ctype != 'website' and modified_date and modified_date != 'None':
+            self.metatags.append((u'og:updated_time', modified_date))
+            if ctype == u'article':
+                self.metatags.append((u'article:modified_time', modified_date))
+
+        if ctype == u'article':
             pub_date = self.context.EffectiveDate()
             if pub_date and pub_date != 'None':
-                self.metatags.append(('article:published_time', pub_date))
-
-            modified_date = self.context.ModificationDate()
-            if modified_date and modified_date != 'None':
-                self.metatags.append(('article:modified_time', modified_date))
+                self.metatags.append((u'article:published_time', pub_date))
 
             expires_date = self.context.ExpirationDate()
             if expires_date and expires_date != 'None':
-                self.metatags.append(('article:expiration_time', expires_date))
+                self.metatags.append((u'article:expiration_time', expires_date))
 
             creator = self.context.Creator()
             if creator:
-                self.metatags.append(('article:author',
+                self.metatags.append((u'article:author',
                                      creator.decode('utf8')))
 
-            section = self.section
-            if section:
-                self.metatags.append(('article:section', section))
+            if self.context.portal_type == 'News Item':
+                self.metatags.append((u'article:section', u'news'))
+            elif self.context.portal_type == 'Event':
+                self.metatags.append((u'article:section', u'event'))
+            else:
+                section = self.section
+                if section:
+                    self.metatags.append((u'article:section', section))
 
             for tag in self.context.Subject():
-                self.metatags.append(('article.tag', tag.decode('utf8')))
+                if tag:
+                    self.metatags.append((u'article:tag', tag.decode('utf8')))
 
     @property
     def image_url(self):
@@ -72,7 +91,7 @@ class OpenGraphTagViewlet(ViewletBase):
             if field and field.get_size(context) > 0:
                 return u'%s/%s_%s' % (obj_url, field.getName(), 'thumb')
 
-        return u"%s/logo.jpg" % self.portal.absolute_url()
+        return u"%s/isaw_logo.png" % self.portal.absolute_url()
 
     @property
     def description(self):
