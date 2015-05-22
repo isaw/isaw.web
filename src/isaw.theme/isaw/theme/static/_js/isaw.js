0e9bfdbee6150d9fc7c55da2ecdfc226ed86e9fa
Alec Mitchell <alecpm@gmail.com>
2015-05-21 18:05:03 -0700

    Navigation portlet fixes.

--- src/isaw.theme/isaw/theme/static/_js/isaw.js
+++ src/isaw.theme/isaw/theme/static/_js/isaw.js
@@ -3,4 +3,11 @@ jQuery(function($) {
 		$(this).toggleClass('open');
 		$('#main-navigation').toggleClass('open');
 	});
+    var nav_folders = $('ul.navTree li.navTreeFolderish');
+    nav_folders.each(function (i, e) {
+        var folder = $(e);
+        if (folder.children().length < 2) {
+            folder.addClass('collapsed');
+        }
+    });
 });
