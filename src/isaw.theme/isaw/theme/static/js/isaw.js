59113352b3d771112640123897eb873498cba517
Alec Mitchell <alecpm@gmail.com>
2015-05-22 13:10:11 -0700

    Give up on dynamic responsiveness for slider for now.

--- src/isaw.theme/isaw/theme/static/js/isaw.js
+++ src/isaw.theme/isaw/theme/static/js/isaw.js
@@ -17,16 +17,17 @@ jQuery(function($) {
         var slide_count = $slides.length;
 
         $slides.css('max-width', $(window).width());
+        $slides.css('height', 'auto');
 
         $slide_list.width($slides.width()*slide_count);
         $slide_list.css('max-width', 100*slide_count + '%');
         $slides.css('max-width', 100/slide_count + '%');
         $slides.css('max-width', 100/slide_count + '%');
-        $slides.css('max-height', $slides.height());
 
         $slider_containers.css('max-width', $(window).width());
         $slider_containers.css('max-height', $slides.height());
     }
+    // Ideally we would listen for window resizes, but EasySlider sets the
+    // width for scrolling one time only.
     resize_slider();
-    $( window ).resize(resize_slider);
 });
