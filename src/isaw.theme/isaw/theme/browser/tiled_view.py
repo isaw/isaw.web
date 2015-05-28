f96e833d1f1ccef99dd4382d7d05955f56cacab3
cewing <cris@crisewing.com>
2015-05-27 19:29:37 -0700

    nearing completion of the news listing

--- src/isaw.theme/isaw/theme/browser/tiled_view.py
+++ src/isaw.theme/isaw/theme/browser/tiled_view.py
@@ -2,8 +2,12 @@
 """View providing a tiled layout for listed items
 
 Suitable for folders or collections
+
+Also includes a viewlet which will show the "featured_item" returned by 
+the method on the view.
 """
 from DateTime import DateTime
+from plone.app.layout.viewlets import common as base
 from Products.CMFCore.utils import getToolByName
 from Products.Five.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
@@ -12,9 +16,7 @@ from zope.interface import implements
 from isaw.theme.browser.interfaces import ITiledListingView
 
 
-class TiledListingView(BrowserView):
-    """view class"""
-    implements(ITiledListingView)
+class TileDetailsMixin(object):
 
     _translation_service = None
     _membership_tool = None
@@ -35,48 +37,13 @@ class TiledListingView(BrowserView):
             )
         return self._membership_tool
 
-    def listings(self, batch=True, b_start=1, b_size=9, sort_on=None, **kwargs):
-        """get a page of listings"""
-        if self.context.portal_type == 'Folder':
-            content_filter = {
-                'sort_on': sort_on,
-                'b_start': b_start,
-                'b_size': b_size,
-            }
-            content_filter.update(kwargs)
-            items = self.context.getFolderContents(
-                content_filter, batch=batch
-            )
-        elif self.context.portal_type == 'Topic':
-            if sort_on:
-                kwargs['sort_on'] = sort_on
-            # topics look for b_start on the request
-            if b_start and not self.request.get('b_start'):
-                self.request['b_start'] = b_start
-            items = self.context.queryCatalog(
-                self.request, batch, b_size, **kwargs
-            )
-        elif self.context.portal_type == 'Collection':
-            items = self.context.results(
-                batch, b_start, b_size, sort_on, **kwargs
-            )
-        else:
-            items = []
-        return items
-
-    def first_item(self):
-        """get the 'first item' for the listing
-
-        Once implemented, this should return the 'sticky' item for this
-        context, or the first item in the listing if the sticky item is not in
-        the listing or is not set.
-        """
-        batch = self.listings(batch=True, b_start=0, b_size=1, sort_on=None)
-        return [i for i in batch][0]
-
     def get_byline(self, brain):
         author_info = self.membership_tool.getMemberInfo(brain.Creator)
-        author = author_info.get('fullname') or author_info.get('username') or brain.Creator
+        author = ''
+        if author_info:
+            author = author_info.get('fullname') or author_info.get('username')
+        if author == '':
+            author = brain.Creator
         parts = [author]
         date = brain.EffectiveDate
         if date:
@@ -86,8 +53,70 @@ class TiledListingView(BrowserView):
     def get_image(self, brain):
         scales = self.context.restrictedTraverse(brain.getPath() + '/@@images')
         try:
-            scale = scales.scale('image', 'blogtile')
-            return '<img src={} title={} />'.format(scale.url, brain.Title)
+            scale = scales.scale('image', self.image_scale)
+            return '<img src={} alt={} />'.format(scale.url, brain.Title)
         except (TypeError, AttributeError):
             # no image available, use the placeholder div
-            return '<div class="img_placeholder">&nbsp;</div>'
+            return self.image_placeholder
+
+
+
+class TiledListingView(BrowserView, TileDetailsMixin):
+    """view class"""
+    implements(ITiledListingView)
+    image_scale = 'blogtile'
+    image_placeholder = '<div class="blogtile_placeholder">&nbsp;</div>'
+    batch_size = 9
+    page = 1
+
+    def __init__(self, request, context):
+        super(TiledListingView, self).__init__(request, context)
+        self.page = int(self.request.get('page', 1))
+
+    def listings(self, b_start=None, b_size=None):
+        """get a page of listings"""
+        if b_size == None:
+            b_size = self.batch_size
+        if b_start == None:
+            b_start = (getattr(self, 'page', 1) - 1) * b_size + 1
+        if self.context.portal_type == 'Folder':
+            content_filter = {
+                'b_start': b_start,
+                'b_size': b_size,
+            }
+            items = self.context.getFolderContents(
+                content_filter, batch=True
+            )
+        elif self.context.portal_type == 'Topic':
+            if b_start and not self.request.get('b_start'):
+                self.request['b_start'] = b_start
+            items = self.context.queryCatalog(self.request, True, b_size)
+        elif self.context.portal_type == 'Collection':
+            items = self.context.results(True, b_start, b_size)
+        else:
+            items = []
+        return items
+
+    def featured_item(self):
+        """get the 'featured item' for the listing
+
+        At this point, this is always the first item in the listing.
+
+        Once implemented, this should return the 'sticky' item for this
+        context, or the first item in the listing if the sticky item is not in
+        the listing or is not set.
+        """
+        batch = self.listings(b_start=0, b_size=1)
+        return [i for i in batch][0]
+
+
+class FeaturedItemViewlet(base.ViewletBase, TileDetailsMixin):
+    index = ViewPageTemplateFile('templates/featured_item.pt')
+    image_scale = 'featured'
+    image_placeholder = '<div class="featured_placeholder">&nbsp;</div>'
+
+    def update(self):
+        self.show = ITiledListingView.providedBy(self.view)
+
+    def get_item(self):
+        return self.view.featured_item()
