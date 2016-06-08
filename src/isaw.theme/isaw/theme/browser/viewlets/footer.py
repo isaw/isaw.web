01fb5887ddf4ae558637e17499638fbc5288acf0
Carlos de la Guardia <cguardia@yahoo.com>
2016-06-08 03:54:14 -0500

    move personal tools links to viewlet to avoid conflict with theme rules

--- src/isaw.theme/isaw/theme/browser/viewlets/footer.py
+++ src/isaw.theme/isaw/theme/browser/viewlets/footer.py
@@ -1,5 +1,6 @@
+from Acquisition import aq_inner
 from plone.registry.interfaces import IRegistry
-from zope.component import getUtility
+from zope.component import getUtility, getMultiAdapter
 
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plone.app.layout.viewlets.common import ViewletBase
@@ -11,7 +12,19 @@ class SiteFooter(ViewletBase):
     render = ViewPageTemplateFile('footer.pt')
 
     def html(self):
+        context = aq_inner(self.context)
+        context_state = getMultiAdapter((context, self.request),
+                                        name=u'plone_context_state')
+        user_actions = context_state.actions('user')
+        actions_html = '<ul>'
+        for action in user_actions:
+            actions_html += '<li id="personaltools-{}">'.format(action['id'])
+            actions_html += '<a href="{}">{}</a>'.format(action['url'],
+                                                         action['title'])
+            actions_html += '</li>'
+        actions_html += '</ul>'
         registry = getUtility(IRegistry)
         settings = registry.forInterface(IISAWSettings, False)
         html = getattr(settings, 'footer_html', u'')
+        html = html.replace('<ul id="personal-tools-links"></ul>', actions_html)
         return html
