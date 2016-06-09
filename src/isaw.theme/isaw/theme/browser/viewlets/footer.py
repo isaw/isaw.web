3c4c42f1298af4cce1b3f563ec69b613a04efd57
Carlos de la Guardia <cguardia@yahoo.com>
2016-06-09 00:56:20 -0500

    add user name to footer user actions

--- src/isaw.theme/isaw/theme/browser/viewlets/footer.py
+++ src/isaw.theme/isaw/theme/browser/viewlets/footer.py
@@ -2,6 +2,7 @@ from Acquisition import aq_inner
 from plone.registry.interfaces import IRegistry
 from zope.component import getUtility, getMultiAdapter
 
+from Products.CMFCore.utils import getToolByName
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plone.app.layout.viewlets.common import ViewletBase
 
@@ -15,7 +16,11 @@ class SiteFooter(ViewletBase):
         context = aq_inner(self.context)
         context_state = getMultiAdapter((context, self.request),
                                         name=u'plone_context_state')
+        portal_state = getMultiAdapter((context, self.request),
+                                       name=u'plone_portal_state')
         user_actions = context_state.actions('user')
+        anonymous = portal_state.anonymous()
+
         actions_html = '<ul>'
         for action in user_actions:
             actions_html += '<li id="personaltools-{}">'.format(action['id'])
@@ -23,6 +28,26 @@ class SiteFooter(ViewletBase):
                                                          action['title'])
             actions_html += '</li>'
         actions_html += '</ul>'
+
+        if not anonymous:
+            member = portal_state.member()
+            userid = member.getId()
+
+            homelink_url = "%s/useractions" % portal_state.navigation_root_url()
+            membership = getToolByName(context, 'portal_membership')
+            member_info = membership.getMemberInfo(userid)
+            if member_info:
+                fullname = member_info.get('fullname', '')
+            else:
+                fullname = None
+            if fullname:
+                username = fullname
+            else:
+                username = userid
+
+            actions_html += '<p><img src="user.png" /> <em>{}</em></p>'.format(
+                username)
+
         registry = getUtility(IRegistry)
         settings = registry.forInterface(IISAWSettings, False)
         html = getattr(settings, 'footer_html', u'')
