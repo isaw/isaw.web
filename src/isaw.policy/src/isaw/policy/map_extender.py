6dd76ee347464ab35ec3cbbd4b1fdcee9548eb05
Alec Mitchell <alecpm@gmail.com>
2016-09-12 14:43:08 -0700

    Update description

--- src/isaw.policy/src/isaw/policy/map_extender.py
+++ src/isaw.policy/src/isaw/policy/map_extender.py
@@ -39,7 +39,7 @@ class MapSchemaExtender(object):
             validators=('isGeoLocation',),
             widget=LocationWidget(
                 label='Geolocation',
-                description="Enter a latitude and longitude"),
+                description="Enter a latitude and longitude in signed decimal degrees. Geodetic model is assumed to be WGS-1984."),
             schemata='Geolocation',
             ),
         ]
@@ -55,7 +55,7 @@ class IGeolocationBehavior(model.Schema):
     """Form field for geolocation behavior"""
     geolocation = GeolocationField(
         title=u"Geolocation",
-        description=u"Enter a latitude and longitude",
+        description=u"Enter a latitude and longitude in signed decimal degrees. Geodetic model is assumed to be WGS-1984.",
         required=False)
     directives.omitted('geolocation')
     directives.no_omit(IEditForm, 'geolocation')
