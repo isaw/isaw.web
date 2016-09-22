c2dda695ef0b53b6acb5718cdd77c573ba32d7a1
Alec Mitchell <alecpm@gmail.com>
2016-09-22 11:15:27 -0700

    Add publication type.

--- src/isaw.policy/src/isaw/policy/__init__.py
+++ src/isaw.policy/src/isaw/policy/__init__.py
@@ -1,6 +1,14 @@
 # Disable the HomeFolderLocator from plone.app.iterate
+import logging
+from zope.i18nmessageid import MessageFactory
 from plone.app.iterate.containers import HomeFolderLocator
 HomeFolderLocator.available = False
+
+
+# Set up the i18n message factory for our package
+MessageFactory = MessageFactory('isaw.policy')
+
+logger = logging.getLogger('isaw.policy')
 
 
 def initialize(context):
