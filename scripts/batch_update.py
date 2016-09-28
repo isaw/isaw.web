88417aa9f53b67013453c0b4005520ab60bb3058
Carlos de la Guardia <cguardia@yahoo.com>
2016-09-28 12:16:59 -0500

    update field names

--- scripts/batch_update.py
+++ scripts/batch_update.py
@@ -14,7 +14,7 @@ from plone.namedfile.file import NamedBlobImage
 
 FIELDS = ['title',
           'description',
-          'context',
+          'exhibition_context',
           'date',
           'not_before',
           'not_after',
