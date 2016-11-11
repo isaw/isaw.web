c97f57552298b6c5e790a2125ae44f43b3009f53
Jesse Snyder <jesse@rasikaconsulting.com>
2016-11-11 10:33:37 -0800

    clean up

--- src/isaw.facultycv/isaw/facultycv/content/actions.py
+++ src/isaw.facultycv/isaw/facultycv/content/actions.py
@@ -2,32 +2,37 @@ from Products.CMFCore.utils import getToolByName
 
 
 def initial_setup(obj, event):
+    memberdata = getToolByName(obj, "portal_memberdata")
+    membertool = getToolByName(obj, "portal_membership")
+
     # Create profile for initial CV
     obj.invokeFactory("CV", id="cv", title="Curriculum Vitae")
 
     # Set the reference of profile to the CV
-    print "Setting reference to CV"
     cv = obj['cv']
     uid = cv.UID()
-    print "UID OF profile folder " + uid
     obj.setProfileRef(uid)
 
     # Set the reference of the member to the CV
-    print "Setting reference to member"
-    memberdata = getToolByName(obj, "portal_memberdata")
-    membertool = getToolByName(obj, "portal_membership")
-
     if not memberdata.hasProperty("CVReference"):
         memberdata.manage_addProperty(id="CVReference", value="", type="string")
-
     obj.setProfileRef(uid)
     uri = obj.absolute_url()
+
     # Turn on permissions for membertool
-    obj.manage_permission("Manage users", roles=['Manager', 'Authenticated', 'Owner'], acquire = 1)
+    obj.manage_permission(
+        "Manage users",
+        roles=['Manager', 'Authenticated', 'Owner'],
+        acquire=1
+    )
     member = membertool.getAuthenticatedMember()
     member.setMemberProperties(mapping={"CVReference": uri})
+
     # Turn off permissions for membertool
-    obj.manage_permission("Manage users", roles=['Manager', 'Authenticated', 'Owner'], acquire = 0)
+    obj.manage_permission(
+        "Manage users",
+        roles=['Manager', 'Authenticated', 'Owner'],
+        acquire=0)
 
     # set profile UID on corresponding member:
     memberID = obj.getMemberID()
