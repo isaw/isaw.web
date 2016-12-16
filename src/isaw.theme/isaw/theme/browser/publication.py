6a0bc248e5f28b1e43b94cf21068735f0274f5e1
Carlos de la Guardia <cguardia@yahoo.com>
2016-12-16 09:25:55 -0600

    publication listing

--- src/isaw.theme/isaw/theme/browser/publication.py
+++ src/isaw.theme/isaw/theme/browser/publication.py
@@ -45,3 +45,53 @@ class PublicationImagesView(BrowserView):
     @property
     def images(self):
         return self.context.objectValues()
+
+
+class PublicationListingView(BrowserView):
+    """view class"""
+    batch_size = 0
+    page = 1
+
+    def __init__(self, request, context):
+        super(PublicationListingView, self).__init__(request, context)
+        self.page = int(self.request.get('page', 1))
+
+    def _query(self, query=None, exclude=None, b_start=None, b_size=None):
+        if b_size is None:
+            b_size = self.batch_size
+        if b_start is None:
+            b_start = (getattr(self, 'page', 1) - 1) * b_size
+
+        if query is None:
+            query = {'portal_type': 'isaw.policy.publication'}
+
+        if exclude is not None:
+            uuid = getattr(exclude, 'UID')
+            if callable(uuid):
+                uuid = uuid()
+            if uuid:
+                query['UID'] = {'not': uuid}
+
+        if self.context.portal_type == 'Folder':
+            self.request['b_start'] = b_start
+            self.request['b_size'] = b_size
+            query['b_start'] = b_start
+            query['b_size'] = b_size
+            items = self.context.getFolderContents(contentFilter=query,
+                                                   batch=True, b_size=b_size)
+        elif self.context.portal_type == 'Topic':
+            if b_start and not self.request.get('b_start'):
+                self.request['b_start'] = b_start
+            items = self.context.queryCatalog(self.request, True, b_size,
+                                              **query)
+        elif self.context.portal_type == 'Collection':
+            items = self.context.results(True, b_start, b_size,
+                                         custom_query=query)
+        else:
+            items = []
+
+        return items
+
+    def listings(self, b_start=None, b_size=None):
+        """get a page of listings"""
+        return self._query(b_start=b_start, b_size=b_size)
