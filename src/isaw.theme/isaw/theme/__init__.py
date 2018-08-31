e95ea3df1153172356f210391dd23d43cbd8d959
Carlos de la Guardia <cguardia@yahoo.com>
2018-08-30 20:10:38 -0500

    monkey patch image field tag to not show title, update event listing for forked contentleadimage

--- src/isaw.theme/isaw/theme/__init__.py
+++ src/isaw.theme/isaw/theme/__init__.py
@@ -1,5 +1,7 @@
 from .patches import allow_not_uuid
+from .patches import img_tag_no_title
 
 
 def initialize(context):
     allow_not_uuid()
+    img_tag_no_title()
--- src/isaw.theme/isaw/theme/patches.py
+++ src/isaw.theme/isaw/theme/patches.py
@@ -3,3 +3,50 @@ from Products.PluginIndexes.UUIDIndex.UUIDIndex import UUIDIndex
 
 def allow_not_uuid():
     UUIDIndex.query_options = tuple(UUIDIndex.query_options) + ('not',)
+
+
+def _wcag_tag(self, instance, scale=None, height=None, width=None, alt=None,
+              css_class=None, title=None, **kwargs):
+    """"Accept title as parameter, but do not use in img tag, per WCAG"""
+    image = self.getScale(instance, scale=scale)
+    if image:
+        img_width, img_height = self.getSize(instance, scale=scale)
+    else:
+        img_height = 0
+        img_width = 0
+
+    if height is None:
+        height = img_height
+    if width is None:
+        width = img_width
+
+    url = instance.absolute_url()
+    if scale:
+        url += '/' + self.getScaleName(scale)
+    else:
+        url += '/' + self.getName()
+
+    if alt is None:
+        alt = instance.Title()
+
+    values = {'src': url,
+              'alt': escape(alt, quote=True),
+              'height': height,
+              'width': width,
+              }
+
+    result = '<img src="%(src)s" alt="%(alt)s" '\
+                'height="%(height)s" width="%(width)s"' % values
+
+    if css_class is not None:
+        result = '%s class="%s"' % (result, css_class)
+
+    for key, value in kwargs.items():
+        if value:
+            result = '%s %s="%s"' % (result, key, value)
+
+    return '%s />' % result
+
+
+def img_tag_no_title():
+    ImageField.tag = _wcag_tag
