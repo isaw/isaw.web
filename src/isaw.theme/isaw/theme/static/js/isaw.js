23127d759b8e6e22c20a4707fda4f1e839614ae6
Alec Mitchell <alecpm@gmail.com>
2019-09-11 12:11:37 -0700

    Set an explicit login context for redirects.

--- src/isaw.theme/isaw/theme/static/js/isaw.js
+++ src/isaw.theme/isaw/theme/static/js/isaw.js
@@ -337,5 +337,10 @@ jQuery(function($) {
             text = text.substr(0,50).split(" ").slice(0, -1).join(" ") + '\u2026'
         }
         $breadcrumb.text(text);
-    })
+    });
+    $('a[id^="personaltools-login"]').click(function () {
+        if (window.createCookie !== undefined) {
+            window.createCookie('came_from', window.location.href);
+        }
+    });
 });
