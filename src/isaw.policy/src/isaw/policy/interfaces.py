0e5f7fb80a941c9fcfb548d3cdd78da300eab97b
Jesse Snyder <jsnyder@wesleyan.edu>
2019-09-13 13:35:37 -0700

    Update title field names for sanity

--- src/isaw.policy/src/isaw/policy/interfaces.py
+++ src/isaw.policy/src/isaw/policy/interfaces.py
@@ -2,13 +2,11 @@ from zope.interface import Interface
 from zope.interface import Invalid
 from zope.interface import invariant
 from zope.schema import List, Text, TextLine, Tuple, URI
-from isaw.bibitems.interfaces import IBibliographicItem
 from isaw.policy import MessageFactory as _
 from plone.app.textfield import RichText
 from plone.dexterity.browser import add
 from plone.directives import form
 from plone.namedfile import field as namedfile
-from z3c.form import widget
 from z3c.form.browser.textlines import TextLinesFieldWidget
 
 
@@ -18,11 +16,14 @@ class IISAWPolicyLayer(Interface):
 
 class IISAWPublication(form.Schema):
     title = TextLine(
-        title=_(u"Title"),
-        description=_(u"The short title of the bibliographic reference."),
+        title=_(u"Short Title"),
+        description=_(u"The short title of this publication"),
     )
 
-    short_title = TextLine(title=_(u'Short Title'))
+    full_title = TextLine(
+        title=_(u'Full Title'),  # Legacy attribute name is unfortunate. Sorry!
+        description=_(u'The full bibliographic title of the publication'),
+    )
 
     description = Text(
         title=_(u'Summary'),
@@ -165,17 +166,6 @@ class PublicationAddForm(add.DefaultAddForm):
 
 class PublicationAddView(add.DefaultAddView):
     form = PublicationAddForm
-
-
-PubTitleLabel = widget.StaticWidgetAttribute(
-    u'Full Title',
-    context=IISAWPublication, field=IBibliographicItem['title']
-)
-
-PubAddTitleLabel = widget.StaticWidgetAttribute(
-    u'Full Title',
-    view=PublicationAddForm, field=IBibliographicItem['title']
-)
 
 
 class IISAWLibCollection(form.Schema):
