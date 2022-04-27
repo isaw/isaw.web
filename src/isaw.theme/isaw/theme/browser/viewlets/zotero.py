940b8335f8c0c7942c6f09d4bc860fc005e49881
Alec Mitchell <alecpm@gmail.com>
2022-04-26 23:18:10 -0700

    Update regexp for zotero API url generation.

--- src/isaw.theme/isaw/theme/browser/viewlets/zotero.py
+++ src/isaw.theme/isaw/theme/browser/viewlets/zotero.py
@@ -4,7 +4,7 @@ from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plone.app.layout.viewlets.common import ViewletBase
 
 ZOTERO_JSON_BASE = 'https://api.zotero.org{}?v=3&format=json'
-Z_MATCH = re.compile(r'^/(groups|users)/\d+/items/[A-Z1-9]+$')
+Z_MATCH = re.compile(r'^/(groups|users)/(\d+)/(\w+/)?items/([A-Z1-9]+)$')
 
 
 class PublicationZoteroViewlet(ViewletBase):
@@ -21,4 +21,5 @@ class PublicationZoteroViewlet(ViewletBase):
         domain = parsed.netloc
         if domain == 'www.zotero.org' and Z_MATCH.match(zotero_path):
             self.html_ref = zotero_url
-            self.json_ref = ZOTERO_JSON_BASE.format(zotero_path)
+            api_path = Z_MATCH.sub(r'/\1/\2/items/\4', zotero_path)
+            self.json_ref = ZOTERO_JSON_BASE.format(api_path)
